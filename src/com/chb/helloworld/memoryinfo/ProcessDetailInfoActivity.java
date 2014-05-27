de49e20e675c0155160516dd0fe7e333d0bb6246
陈红波 <hongbo.chen@renren-inc.com>
2014-05-27 14:56:29 +0800

    增加获取 packageInfo 功能

--- src/com/chb/helloworld/memoryinfo/ProcessDetailInfoActivity.java
+++ src/com/chb/helloworld/memoryinfo/ProcessDetailInfoActivity.java
@@ -1,9 +1,18 @@
 package com.chb.helloworld.memoryinfo;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import com.chb.helloworld.R;
+
+import java.io.ByteArrayInputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
 /**
  * Created by renen-inc_hempel on 14-5-27.
@@ -49,5 +58,31 @@ public class ProcessDetailInfoActivity extends Activity {
 		tvUID.setText(processInfo.getUid() + "");
 		tvProcessMemSize.setText(processInfo.getMemSize() + "KB");
 		tvProcessName.setText(processInfo.getProcessName());
+
+		getSingInfo(processInfo.getProcessName());
+	}
+
+	public void getSingInfo(String packageName) {
+		try {
+			PackageInfo packageInfo = getPackageManager().getPackageInfo(packageName, PackageManager.GET_SIGNATURES);
+			Signature[] signs = packageInfo.signatures;
+			Signature sign = signs[0];
+			parseSignature(sign.toByteArray());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void parseSignature(byte[] signature) {
+		try {
+			CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+			X509Certificate cert = (X509Certificate) certFactory.generateCertificate(new ByteArrayInputStream(signature));
+			String pubKey = cert.getPublicKey().toString();
+			String signNumber = cert.getSerialNumber().toString();
+			Log.d("CHB", "pubKey  = " + pubKey);
+			Log.d("CHB", "signNumber  = " + signNumber);
+		} catch (CertificateException e) {
+			e.printStackTrace();
+		}
 	}
 }
