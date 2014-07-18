7067c7be60c429aef49f9e6bfd3bf7b46d523010
陈红波 <hongbo.chen@renren-inc.com>
2014-07-18 17:36:47 +0800

    采用列表方式展示好友

--- src/com/chb/helloworld/im/FormFriendsList.java
+++ src/com/chb/helloworld/im/FormFriendsList.java
@@ -1,7 +1,19 @@
 package com.chb.helloworld.im;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import com.chb.helloworld.R;
 import com.chb.helloworld.im.util.XmppTool;
 import org.jivesoftware.smack.Roster;
 import org.jivesoftware.smack.RosterEntry;
@@ -18,28 +30,64 @@ import java.util.List;
  */
 
 public class FormFriendsList extends Activity {
+	private List<RosterEntry> rosterList;
+	private ListView listView;
+	private IMFriendListAdapter adapter;
+
+	private final int MSG_TO_GETDATA = 1;
+	private final int MSG_TO_SETUI = 2;
+	private final int MSG_TO_UPDATEUI = 3;
+
+
+
+	Handler handler = new Handler(){
+		public void handleMessage(Message msg){
+			switch (msg.what) {
+				case MSG_TO_GETDATA:
+					getData();
+					break;
+
+				case MSG_TO_SETUI:
+					setUI();
+					break;
+
+				case MSG_TO_UPDATEUI:
+					break;
+			}
+		}
+	};
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.im_formfriendslist);
+		listView = (ListView)findViewById(R.id.friendslist);
 
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				getData();
-			}
-		}).start();
+//		new Thread(new Runnable() {
+//			@Override
+//			public void run() {
+//				getData();
+//			}
+//		}).start();
+		handler.sendEmptyMessage(MSG_TO_GETDATA);
 	}
 
 
 	public List<RosterEntry> getData() {
 		Roster roster = XmppTool.getConnection().getRoster();
-		List<RosterEntry> list = new ArrayList<RosterEntry>();
+		rosterList = new ArrayList<RosterEntry>();
 		Collection<RosterEntry> rosterEntries = roster.getEntries();
 		Iterator<RosterEntry> iterator = rosterEntries.iterator();
 		while (iterator.hasNext()){
-			list.add(iterator.next());
+			rosterList.add(iterator.next());
 		}
+		handler.sendEmptyMessage(MSG_TO_SETUI);
+		return rosterList;
+	}
 
-		return list;
+	public void setUI(){
+		adapter = new IMFriendListAdapter(this);
+		listView.setAdapter(adapter);
+		adapter.notifyDataSetChanged();
 	}
 
 	@Override
@@ -50,4 +98,59 @@ public class FormFriendsList extends Activity {
 		}
 
 	}
+
+	class IMFriendListAdapter extends BaseAdapter {
+		private LayoutInflater mInflater;
+
+		IMFriendListAdapter(Context context){
+			mInflater = LayoutInflater.from(context);
+		}
+
+		@Override
+		public int getCount(){
+			return rosterList.size();
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent){
+			System.out.println("getView position=" + position + ", view="+convertView);
+
+			ViewHolder holder = new ViewHolder();
+			if(convertView == null){
+				convertView = (View) mInflater.inflate(R.layout.im_friendslist_item, null);
+
+				holder.header = (ImageView)convertView.findViewById(R.id.friendslist_header);
+				holder.jid = (TextView)convertView.findViewById(R.id.friendslist_jid);
+				holder.nickName = (TextView)convertView.findViewById(R.id.friendslist_name);
+
+				convertView.setTag(holder);
+
+			} else {
+				holder = (ViewHolder)convertView.getTag();
+			}
+			holder.header.setImageDrawable(getResources().getDrawable(R.drawable.default_header));
+			holder.jid.setText(rosterList.get(position).getUser());
+			holder.nickName.setText(rosterList.get(position).getName());
+			return convertView;
+		}
+
+	}
+
+	class ViewHolder {
+		public ImageView header;
+		public TextView jid;
+		public TextView nickName;
+
+	}
 }
+
